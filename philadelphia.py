43616486d27f93007e81c4293ce556bc8ec89109
jer164 <62763243+jer164@users.noreply.github.com>
2023-03-03 17:12:20 -0500

    This commit added code for my collaborator

---
+++ philadelphia.py
@@ -0,0 +1,25 @@
+def philadelphia(candidate):
+    import requests
+    import pandas as pd
+
+    good_names = ["donation_date", "donation_amount", "full_name", "addr1", "city", "state", "zip", \
+    "full_address", "first_name", "middle_name", "last_name", \
+    "addr2",	"phone1", "phone2", "email1", "email2"]
+
+    response = requests.get(f"https://cf-api.phila.gov/services/campaign-finance/search/searchtransactions?query=&limit=10000&ascending=1&page=1&byColumn=0&orderBy=transactionDate&searchString={candidate}&amount=&isSearchByFilingEntity=true&year=&cycleCode=&isSearchOnExpenditure=false")
+    j = response.json()
+    df = pd.json_normalize(j['transactionList'])
+    df = df.rename(columns={'transactionEntityName': 'full_name', 'transactionEntityAddress': 'full_address',  'EntityCity': 'city', \
+        'transactionDate': 'donation_date', 'transactionAmount': 'donation_amount'})
+
+    for col in df.columns.values:
+        if col not in good_names:
+            df.drop(col, axis=1, inplace=True)
+            
+    for col in good_names:
+        if col not in df.columns.values:
+            df[col] = ""
+
+    df['donation_date'] = df['donation_date'].astype(str).str[0:10]
+    
+    return df
