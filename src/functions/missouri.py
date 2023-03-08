1b74820348eb91b63720ed4a9c5109b6e60ed6e8
Ivy Wong <xy.ivywong@gmail.com>
2023-03-08 18:56:22 -0500

    Add docstring to missouri function to explain function

--- src/functions/missouri.py
+++ src/functions/missouri.py
@@ -5,6 +5,18 @@ import pandas as pd
 
 def missouri(input_path):
 
+    """
+    Reads an HTML file and returns a pandas DataFrame with the contents.
+
+    Args:
+        input_path (str): the path to the input HTML file
+
+    Returns:
+        pandas.DataFrame: a DataFrame containing the contents of the HTML file
+   
+    """
+
+
     with open(input_path) as html:
         df = pd.read_html(html)[0]
 
