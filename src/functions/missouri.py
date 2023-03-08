109c13c294c2e0b9b89192d82bce655d398e2928
Ivy Wong <xy.ivywong@gmail.com>
2023-03-08 18:49:21 -0500

    Reorganize imports outside missouri function

--- src/functions/missouri.py
+++ src/functions/missouri.py
@@ -1,6 +1,9 @@
-def missouri(input_path):
+# Putting the imports outside the function instead. This helps to avoid importing the same module 
+# multiple times if the function is called multiple times.
 
-    import pandas as pd
+import pandas as pd
+
+def missouri(input_path):
 
     with open(input_path) as html:
         df = pd.read_html(html)[0]
